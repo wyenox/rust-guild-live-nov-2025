876ceb0b28b7860c38326a5273ff8999bbe3395b
wyenox <227889946+wyenox@users.noreply.github.com>
2025-11-21 15:04:38 -0300

    split

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,44 @@
-use rand::prelude::*;
-
-struct TextoChiquito(String);
-
-impl From<&str> for TextoChiquito {
-    fn from(s: &str) -> Self {
-        TextoChiquito(s.to_string())
-    }
+struct StrSplit<'delimiter, 'document> {
+    index: usize,
+    delimiter: &'delimiter str,
+    document: &'document str,
 }
 
-struct Menu {
-    platos: Vec<TextoChiquito>,
-}
-
-struct Lentes;
-
-impl Lentes {
-    fn leer<'lentes, 'texto>(&'lentes self, texto: &'texto TextoChiquito) -> &'texto str {
-        &texto.0
-    }
-
-    fn romper(self) {}
-}
-
-struct Humano<'menu, 'lentes> {
-    mambos_mentales: StdRng,
-    lentes: &'lentes Lentes,
-    menu: &'menu Menu,
-}
-
-impl<'menu, 'lentes> Humano<'menu, 'lentes> {
-    fn leer_menu_y_decidir(&mut self) -> &'menu str {
-        match self.menu.platos.choose(&mut self.mambos_mentales) {
-            Some(plato) => self.lentes.leer(plato),
-            None => "Aire,",
+impl<'delimiter, 'document> Iterator for StrSplit<'delimiter, 'document> {
+    type Item = &'document str;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index >= self.document.len() {
+            return None;
+        }
+        match self.document[self.index..].find(self.delimiter) {
+            Some(pos) => {
+                let result = &self.document[self.index..self.index + pos];
+                self.index += pos + self.delimiter.len();
+                Some(result)
+            }
+            None => {
+                let result = &self.document[self.index..];
+                self.index = self.document.len();
+                Some(result)
+            }
         }
     }
 }
 
+fn str_before(s: &str, c: char) -> Option<&str> {
+    StrSplit {
+        index: 0,
+        document: s,
+        delimiter: &c.to_string(),
+    }
+    .next()
+}
+
 fn main() {
-    let lentes = Lentes;
-    let menu = Menu {
-        platos: vec![
-            "Chivito".into(),
-            "Asado".into(),
-            "Panchos".into(),
-            "Milanesa".into(),
-            "Hamburguesa".into(),
-            "Vino Tinto".into(),
-        ],
-    };
-
-    let mut humano = Humano {
-        mambos_mentales: StdRng::from_os_rng(),
-        lentes: &lentes,
-        menu: &menu,
-    };
-
-    let eleccion = humano.leer_menu_y_decidir();
-    lentes.romper();
-
-    println!("Me gustaria pedir \"{}\", por favor", eleccion);
+    let document = "Here we have a huge document, that explains a lot of things.";
+    let delim = ',';
+    match str_before(document, delim) {
+        Some(text) => println!("Text before {} = {}", delim, text),
+        None => println!("There's no text before {}", delim),
+    }
 }
